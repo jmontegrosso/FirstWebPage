c17a97693cdd80c4ff62cb01d6309f4aba18cb38
Jeremías Montegrosso <jeremontegrosso@gmail.com>
2024-02-24 19:50:59 +0100

    First web page

---
+++ js/unidad7.js
@@ -0,0 +1,11 @@
+var nombreUsuario = prompt('Dime ¿Cúal es tu nombre?')
+var esViajero = prompt('¿Te gustan los viajes?')
+var tipoViaje = prompt('¿Que tipos de viajes te interesan?')
+var recibeInformacion = prompt('¿Te gustaria recibit informacion de viajes en tu e-mail?')
+var eMail = prompt('Ingrese su e-mail para recibor nuestras novedades')
+alert('Prximamente recibiras novedades. Gracias por tu tiempo '+nombreUsuario+'.')
+
+// SUMA +
+// RESTAR -
+// MULTIPLICAR *
+// DIVIDIR /
---
+++ js/unidad8.js
@@ -0,0 +1,32 @@
+function mostrarTextoJs() {    
+    var caja = document.getElementById('servicio_text_js')
+    // el elemento no se carga
+    // caja.style.display = 'none'
+    if(caja.style.display == 'none'){
+        caja.style.display = 'block'
+    } else {
+        caja.style.display = 'none' 
+    }
+}  
+
+function mostrarTextoScooter() {    
+    var caja = document.getElementById('servicio_text_scooter')
+    // el elemento no se carga
+    // caja.style.display = 'none'
+    if(caja.style.display == 'none'){
+        caja.style.display = 'block'
+    } else {
+        caja.style.display = 'none' 
+    }
+}  
+
+function mostrarTextoPasaporte() {    
+    var caja = document.getElementById('servicio_text_pasaporte')
+    // el elemento no se carga
+    // caja.style.display = 'none'
+    if(caja.style.display == 'none'){
+        caja.style.display = 'block'
+    } else {
+        caja.style.display = 'none' 
+    }
+}   
